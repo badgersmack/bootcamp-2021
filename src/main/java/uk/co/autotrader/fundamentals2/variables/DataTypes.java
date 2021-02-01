69765e30768e99296aa2e4017985f5119b9a2525
badgersmack <luke_chant@yahoo.co.uk>
2021-02-01 15:27:25 +0000

    add variable examples and data type examples

---
+++ src/main/java/uk/co/autotrader/fundamentals2/variables/DataTypes.java
@@ -0,0 +1,39 @@
+package uk.co.autotrader.fundamentals2.variables;
+
+import java.util.Date;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        Date dateOfbirth = new Date();
+
+        System.out.println("dateOfbirth = " + dateOfbirth);
+
+        // Primitive
+        boolean isItRaining = false;
+        System.out.println("isItRaining = " + isItRaining);
+
+        byte MyByte = 1; // 1 byte = 8 bits
+        System.out.println("MyByte = " + MyByte);
+
+        short MyShort = 123; //range -32768 to 32768 which equals 2 bytes
+        System.out.println("MyShort = " + MyShort);
+
+        int MyInt = 123; // numbers in the billions
+        System.out.println("MyInt = " + MyInt);
+
+        long MyLong = 99L; // lots
+        System.out.println("MyLong = " + MyLong);
+
+        float MyFloat = 17.64f; //4 bytes
+        System.out.println("MyFloat = " + MyFloat);
+
+        double myDouble = 99.99d; //preferred over floats
+        System.out.println("myDouble = " + myDouble);
+
+        char gender = 'f'; //single character  16 bit unicode value
+        System.out.println("gender = " + gender);
+
+
+        // Non-primitive
+    }
+}
---
+++ src/main/java/uk/co/autotrader/fundamentals2/variables/SimpleProgram.java
@@ -0,0 +1,25 @@
+package uk.co.autotrader.fundamentals2.variables;
+
+public class SimpleProgram { //Pascal Case
+
+    public static void main(String[] args) {
+
+        // <data type> <variable name>
+        int numberOfStudentsInClassroom = 10;
+
+        System.out.println("numberOfStudentsInClassroom = " + numberOfStudentsInClassroom );
+
+        int num1, num2, num3;
+
+        /*
+        This is discouraged
+         */
+
+        int num4;
+        int num5;
+        // preferred
+        int num6 = 10; //is ok
+
+    }
+
+}
