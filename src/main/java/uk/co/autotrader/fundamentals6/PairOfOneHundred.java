88ad0849564aeaf2a072e591a56f60a330c68491
badgersmack <luke_chant@yahoo.co.uk>
2021-02-12 16:31:20 +0000

    Add updated 100s

--- src/main/java/uk/co/autotrader/fundamentals6/PairOfOneHundred.java
+++ src/main/java/uk/co/autotrader/fundamentals6/PairOfOneHundred.java
@@ -7,15 +7,15 @@ public class PairOfOneHundred {
         int [] scores1 = {1, 100, 100};
         int [] scores2 = {1, 100, 99, 100};
         int [] scores3 = {100, 1, 100, 100};
+        int [] scores4 = {20, 20, 20}; //want this to fail but it passes because there's not check for 100
 
-        int[] demoArray = scores3;
+        int[] demoArray = scores1;
+        boolean pairOfHundreds = false;
         for (int i = 0; i < demoArray.length -1; i++) {
-            if (demoArray[i] == demoArray[i + 1]) {
-                System.out.println("True");
+            if (demoArray[i] == demoArray[i + 1] && demoArray[i] == 100) {
+                pairOfHundreds = true;
             }
-            else {
-                System.out.println("False");
-            }
+            System.out.println(pairOfHundreds);
         }
     }
 }
